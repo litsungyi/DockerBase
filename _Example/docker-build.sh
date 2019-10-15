ef5b99c8548bdf9b40d9f1a2ea331d8086378dba
Li TsungYi <tsungyi.li@aktsk.com>
2019-10-15 18:49:56 +0800

    Add example

---
+++ _Example/docker-build.sh
@@ -0,0 +1,1 @@
+docker build -t xxx-base .
---
+++ _Example/docker-run.sh
@@ -0,0 +1,7 @@
+docker run -it --rm \
+    --name xxx-host \
+    -p 10000:80 \
+    -v "$PWD"/app:/usr/src/app \
+    -w /usr/src/app \
+    xxx-base
+    # Command

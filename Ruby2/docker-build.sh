659873efdf48065cc04e11f4f87d97a5d2b70513
litsungyi <litsungyi@gmail.com>
2021-06-04 13:34:28 +0800

    feat: Add Ruby3

---
+++ Ruby2/docker-build.sh
@@ -0,0 +1,1 @@
+docker build -t ruby2-base .
--- Ruby2/docker-run.sh
+++ Ruby2/docker-run.sh
@@ -1,8 +1,7 @@
 docker run -it --rm \
-    --name ruby-host \
+    --name ruby2-host \
     -p 28000:80 \
     -v "$PWD"/app:/usr/src/app \
     -w /usr/src/app \
-    -e LANG=C.UTF-8 \
-    ruby-base \
+    ruby2-base \
     ruby main.rb

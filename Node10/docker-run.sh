c433716037ba1a7952b6cfba00bd9a77f6a89fa0
litsungyi <litsungyi@gmail.com>
2021-06-06 01:59:15 +0800

    feat: Update node.js 10

--- Node10/docker-run.sh
+++ Node10/docker-run.sh
@@ -1,6 +1,7 @@
 docker run -it --rm \
     --name node-host-10 \
-    -p 23000:80 \
+    -p 24000:80 \
     -v "$PWD"/app:/usr/src/app \
     -w /usr/src/app \
-    node-base-10
+    node-base-10 \
+    npm run start

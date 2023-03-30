db509955eac073e2cb38a1289ceaddad4830d1b1
litsungyi <litsungyi@gmail.com>
2023-03-30 09:22:15 +0800

    feat: Add cpp

---
+++ Cpp/app/build.sh
@@ -0,0 +1,1 @@
+g++ -std=c++20 -o ./output/app -I . -I ./include ./src/**/*.cpp && ./output/app
---
+++ Cpp/docker-run.sh
@@ -0,0 +1,7 @@
+docker run -it --rm \
+    --name cpp-host \
+    -p 10000:80 \
+    -v "$PWD"/app:/usr/src/app \
+    -w /usr/src/app \
+    cpp-base \
+    g++ -std=c++20 -o ./output/app -I . -I ./include ./src/**/*.cpp && ./output/app

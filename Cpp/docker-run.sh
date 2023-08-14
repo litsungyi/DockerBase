bd5d935e6a6859a28942a8183eaca107a98823d1
litsungyi <litsungyi@gmail.com>
2023-08-14 13:05:02 +0800

    feat: Update cpp

--- Cpp/docker-run.sh
+++ Cpp/docker-run.sh
@@ -4,4 +4,4 @@ docker run -it --rm \
     -v "$PWD"/app:/usr/src/app \
     -w /usr/src/app \
     cpp-base \
-    g++ -std=c++20 -o ./output/app -I . -I ./include ./src/**/*.cpp && ./output/app
+    g++ -std=c++20 -o ./output/app -I . -I ./include ./src/*.cpp && ./output/app
